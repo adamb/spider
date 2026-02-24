007bf4d019984fd5137c80c7e035ae321112b4e3
Adam Beguelin <adam@beguelin.com>
2026-02-23 18:08:23 -0600

    Update DEVICE_TIMEOUT constant to 45 minutes
    Increased DEVICE_TIMEOUT from 30 minutes to 45 minutes.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 export const TARGET_URL = 'http://lab.spiderplant.com';
 export const CACHE_TTL = 300; // 5 minutes
-export const DEVICE_TIMEOUT = 30 * 60; // 15 minutes - consider device offline if no report
+export const DEVICE_TIMEOUT = 45 * 60; // 45 minutes - consider device offline if no report
 export const FREEZER_PROBE_ID = '4c7525046c96-101252130008001E';
 export const HUMIDITY_PROBE_ID = '4c7525046c96-0e76b286d29e_rh';
 export const DEPTH_PROBE_ID = '44179312cc0f-range';
